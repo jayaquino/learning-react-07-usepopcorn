7925320498af347936862f23f9f0d556c9660c19
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-11-24 18:16:17 -0800

    feature(component): Reusable star component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={4}
+    />
+    <StarRating maxRating={5} size={24} color="red" className="test" />
   </React.StrictMode>
 );
