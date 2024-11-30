7b4f875256cd0b2fb0702b17069f4c051e72eb68
Nelson Aquino Jr. <naquinojr@outlook.com>
2024-11-29 19:10:00 -0800

    feature(useEffect): listening to key press

--- src/App.js
+++ src/App.js
@@ -356,6 +356,23 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     [title]
   );
 
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
